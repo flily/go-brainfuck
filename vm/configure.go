83c27074a72894ce4d1e21e2d5645ce633fb00fe
Flily Hsu <1126143+flily@users.noreply.github.com>
2026-07-08 01:00:48 +0800

    introduce generic configure type, primary used for test

--- vm/configure.go
+++ vm/configure.go
@@ -14,7 +14,34 @@ func (c Configure) IsStandard() bool {
 }
 
 type ConfigureContainer interface {
-	GetBooleanConfigure(Configure) bool
-	GetIntConfigure(Configure) int64
-	GetUintConfigure(Configure) uint64
+	GetBooleanConfigure(Configure) (bool, bool)
+	GetIntConfigure(Configure) (int64, bool)
+	GetUintConfigure(Configure) (uint64, bool)
+}
+
+type GenericConfigure map[Configure]any
+
+func genericGetConfigure[T any](g GenericConfigure, c Configure) (T, bool) {
+	found := false
+	var result T
+	if v, ok := g[c]; ok {
+		if t, ok := v.(T); ok {
+			result = t
+			found = true
+		}
+	}
+
+	return result, found
+}
+
+func (c GenericConfigure) GetBooleanConfigure(conf Configure) (bool, bool) {
+	return genericGetConfigure[bool](c, conf)
+}
+
+func (c GenericConfigure) GetIntConfigure(conf Configure) (int64, bool) {
+	return genericGetConfigure[int64](c, conf)
+}
+
+func (c GenericConfigure) GetUintConfigure(conf Configure) (uint64, bool) {
+	return genericGetConfigure[uint64](c, conf)
 }
---
+++ vm/configure_test.go
@@ -0,0 +1,122 @@
+package vm
+
+import (
+	"testing"
+)
+
+func TestConfigureIsStandard(t *testing.T) {
+	c1 := Configure(0x01020304)
+	if c1.IsStandard() {
+		t.Fatalf("expected c1 to not be standard")
+	}
+
+	c2 := Configure(0x00010203)
+	if !c2.IsStandard() {
+		t.Fatalf("expected c2 to be standard")
+	}
+}
+
+func TestGenericConfigureBoolean(t *testing.T) {
+	generic := GenericConfigure{
+		1: true,
+		2: int64(42),
+		3: uint64(42),
+	}
+
+	v1, ok := generic.GetBooleanConfigure(1)
+	if !ok {
+		t.Fatalf("expected to find boolean configure for 1")
+	}
+
+	if v1 != true {
+		t.Fatalf("expected boolean configure for 1 to be true, got %v", v1)
+	}
+
+	v2, ok := generic.GetIntConfigure(1)
+	if ok {
+		t.Fatalf("expected not to find int configure for 1")
+	}
+
+	if v2 != 0 {
+		t.Fatalf("expected int configure for 1 to be 0, got %v", v2)
+	}
+
+	v3, ok := generic.GetUintConfigure(1)
+	if ok {
+		t.Fatalf("expected not to find uint configure for 1")
+	}
+
+	if v3 != 0 {
+		t.Fatalf("expected uint configure for 1 to be 0, got %v", v3)
+	}
+}
+
+func TestGenericConfigureInt(t *testing.T) {
+	generic := GenericConfigure{
+		1: true,
+		2: int64(42),
+		3: uint64(42),
+	}
+
+	v1, ok := generic.GetIntConfigure(2)
+	if !ok {
+		t.Fatalf("expected to find int configure for 2")
+	}
+
+	if v1 != 42 {
+		t.Fatalf("expected int configure for 2 to be 42, got %v", v1)
+	}
+
+	v2, ok := generic.GetBooleanConfigure(2)
+	if ok {
+		t.Fatalf("expected not to find boolean configure for 2")
+	}
+
+	if v2 != false {
+		t.Fatalf("expected boolean configure for 2 to be false, got %v", v2)
+	}
+
+	v3, ok := generic.GetUintConfigure(2)
+	if ok {
+		t.Fatalf("expected not to find uint configure for 2")
+	}
+
+	if v3 != 0 {
+		t.Fatalf("expected uint configure for 2 to be 0, got %v", v3)
+	}
+}
+
+func TestGenericConfigureUint(t *testing.T) {
+	generic := GenericConfigure{
+		1: true,
+		2: int64(42),
+		3: uint64(42),
+	}
+
+	v1, ok := generic.GetUintConfigure(3)
+	if !ok {
+		t.Fatalf("expected to find uint configure for 3")
+	}
+
+	if v1 != 42 {
+		t.Fatalf("expected uint configure for 3 to be 42, got %v", v1)
+	}
+
+	v2, ok := generic.GetBooleanConfigure(3)
+	if ok {
+		t.Fatalf("expected not to find boolean configure for 3")
+	}
+
+	if v2 != false {
+		t.Fatalf("expected boolean configure for 3 to be false, got %v", v2)
+	}
+
+	v3, ok := generic.GetIntConfigure(3)
+	if ok {
+		t.Fatalf("expected not to find int configure for 3")
+	}
+
+	if v3 != 0 {
+		t.Fatalf("expected int configure for 3 to be 0, got %v", v3)
+	}
+}
