e1e41107c276b9bb8083c99edf841e81bd315c56
Flily Hsu <1126143+flily@users.noreply.github.com>
2026-07-09 03:30:43 +0800

    add command-line main entry

---
+++ cmd/brainfuck/configure.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/flily/go-brainfuck/vm"
+)
+
+const (
+	DefaultMemorySize = 4 * 1024
+	DefaultStackSize  = 128
+)
+
+type MemoryUnitType int
+
+const (
+	MemoryUnitTypeInvalid MemoryUnitType = iota
+	MemoryUnitTypeUint8
+	MemoryUnitTypeUint16
+	MemoryUnitTypeUint32
+	MemoryUnitTypeUint64
+	MemoryUnitTypeInt8
+	MemoryUnitTypeInt16
+	MemoryUnitTypeInt32
+	MemoryUnitTypeInt64
+)
+
+var memoryUnitTypeText = map[MemoryUnitType]string{
+	MemoryUnitTypeUint8:  "uint8",
+	MemoryUnitTypeUint16: "uint16",
+	MemoryUnitTypeUint32: "uint32",
+	MemoryUnitTypeUint64: "uint64",
+	MemoryUnitTypeInt8:   "int8",
+	MemoryUnitTypeInt16:  "int16",
+	MemoryUnitTypeInt32:  "int32",
+	MemoryUnitTypeInt64:  "int64",
+}
+
+func (t *MemoryUnitType) String() string {
+	if text, ok := memoryUnitTypeText[*t]; ok {
+		return text
+	}
+
+	return "unknown"
+}
+
+func (t *MemoryUnitType) Set(value string) error {
+	for k, v := range memoryUnitTypeText {
+		if v == value {
+			*t = k
+			return nil
+		}
+	}
+
+	return fmt.Errorf("invalid memory unit type '%s'", value)
+}
+
+type Configure struct {
+	MemoryUnitType MemoryUnitType
+	MemorySize     uint64
+	StackSize      uint64
+	ReadValueOnEOF int64
+	IgnoreReadEOF  bool
+	RaiseReadEOF   bool
+}
+
+func NewConfigure() *Configure {
+	conf := &Configure{
+		MemoryUnitType: MemoryUnitTypeUint8,
+		MemorySize:     DefaultMemorySize,
+		StackSize:      DefaultStackSize,
+		ReadValueOnEOF: -1,
+		IgnoreReadEOF:  false,
+		RaiseReadEOF:   false,
+	}
+
+	return conf
+}
+
+func (c *Configure) GetBoolean(conf vm.Configure) (bool, bool) {
+	result := false
+	found := true
+	switch conf {
+	case vm.ConfigureReadValueIgnoreOnEOF:
+		result = c.IgnoreReadEOF
+
+	case vm.ConfigureReadEOFRaiseError:
+		result = c.RaiseReadEOF
+
+	default:
+		found = false
+	}
+
+	return result, found
+}
+
+func (c *Configure) GetInt(conf vm.Configure) (int64, bool) {
+	result := int64(0)
+	found := true
+	switch conf {
+	case vm.ConfigureReadValueOnEOF:
+		result = c.ReadValueOnEOF
+
+	default:
+		found = false
+	}
+
+	return result, found
+}
+
+func (c *Configure) GetUint(conf vm.Configure) (uint64, bool) {
+	result := uint64(0)
+	found := true
+	switch conf {
+	default:
+		found = false
+	}
+
+	return result, found
+}
---
+++ cmd/brainfuck/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/flily/go-brainfuck/context"
+	"github.com/flily/go-brainfuck/vm"
+)
+
+func GenericMain[T vm.MemoryUnit](conf *Configure, source string) {
+	fctx, err := context.ReadFile(source)
+	if err != nil {
+		fmt.Printf("error reading file: %v\n", err)
+		return
+	}
+
+	parser := vm.NewParser(fctx)
+	codemap, err := parser.Parse()
+	if err != nil {
+		fmt.Printf("error parsing file:\n%s\n", err)
+		return
+	}
+
+	fmt.Fprintf(os.Stderr, "configure: %+v\n", conf)
+
+	memorySize, stackSize := int(conf.MemorySize), int(conf.StackSize)
+	if conf.MemorySize > uint64(memorySize) {
+		fmt.Printf("warning: memory size too large, using %d\n", memorySize)
+	}
+
+	if conf.StackSize > uint64(stackSize) {
+		fmt.Printf("warning: stack size too large, using %d\n", stackSize)
+	}
+
+	bfvm := vm.New[T](memorySize, stackSize)
+	bfvm.Input = vm.NewReader(os.Stdin, vm.NewLittleEndianEncoder[T]())
+	bfvm.Output = vm.NewWriter(os.Stdout, vm.NewLittleEndianEncoder[T]())
+	bfvm.Configure = conf
+	bfvm.LoadHandlers(vm.GetStandardInstructionSetHandlers[T]())
+	bfvm.LoadCode(codemap)
+
+	err = bfvm.Run()
+	if err != nil {
+		fmt.Printf("error executing code:\n%s\n", err)
+	}
+}
+
+func makeArgumentParser() (*Configure, *flag.FlagSet) {
+	conf := NewConfigure()
+
+	set := flag.NewFlagSet("brainfuck", flag.ExitOnError)
+	set.Var(&conf.MemoryUnitType, "unit-type",
+		"data type for memory unit cell")
+	set.Int64Var(&conf.ReadValueOnEOF, "eof-value", -1,
+		"value returned when meets EOF")
+	set.BoolVar(&conf.IgnoreReadEOF, "ignore-eof", false,
+		"ignore EOF when reading input")
+	set.BoolVar(&conf.RaiseReadEOF, "raise-eof", false,
+		"raise error when meets EOF")
+
+	return conf, set
+}
+
+func main() {
+	rawArgs := os.Args[1:]
+	conf, set := makeArgumentParser()
+	_ = set.Parse(rawArgs)
+	args := set.Args()
+
+	if len(args) <= 0 {
+		fmt.Printf("no args\n")
+		return
+	}
+
+	filename := args[0]
+
+	switch conf.MemoryUnitType {
+	case MemoryUnitTypeUint8:
+		GenericMain[uint8](conf, filename)
+	case MemoryUnitTypeUint16:
+		GenericMain[uint16](conf, filename)
+	case MemoryUnitTypeUint32:
+		GenericMain[uint32](conf, filename)
+	case MemoryUnitTypeUint64:
+		GenericMain[uint64](conf, filename)
+	case MemoryUnitTypeInt8:
+		GenericMain[int8](conf, filename)
+	case MemoryUnitTypeInt16:
+		GenericMain[int16](conf, filename)
+	case MemoryUnitTypeInt32:
+		GenericMain[int32](conf, filename)
+	case MemoryUnitTypeInt64:
+		GenericMain[int64](conf, filename)
+	default:
+		fmt.Printf("invalid memory unit type '%s'\n", conf.MemoryUnitType.String())
+	}
+}
