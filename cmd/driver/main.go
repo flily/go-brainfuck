ba128881f814a58db0dbfaa90c18acbb76e32c78
Flily Hsu <1126143+flily@users.noreply.github.com>
2026-08-21 18:17:12 +0800

    refactor run command in driver

--- cmd/driver/main.go
+++ cmd/driver/main.go
@@ -5,15 +5,44 @@ import (
 	"fmt"
 )
 
-func main() {
-	flag.Parse()
-	args := flag.Args()
+const (
+	CommandRun = "run"
+)
 
-	for _, filename := range args {
+func doRun(args []string) {
+	set := flag.NewFlagSet("run", flag.ExitOnError)
+	_ = set.Parse(args)
+
+	if set.NArg() <= 0 {
+		set.Usage()
+		return
+	}
+
+	for _, filename := range set.Args() {
 		err := runCase(filename)
 		if err != nil {
 			fmt.Printf("%s\n", err)
 			break
 		}
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	if flag.NArg() <= 0 {
+		fmt.Printf("Usage: %s [COMMAND]\n", "go-brainfuck")
+		fmt.Printf("commands:\n")
+		fmt.Printf("  run [FILE]...  run test driver\n")
+		return
+	}
+
+	args := flag.Args()
+	switch args[0] {
+	case CommandRun:
+		doRun(args[1:])
+
+	default:
+		fmt.Printf("Unknown command: %s\n", args[0])
 	}
 }
